11ddea26a7e6169a8b7e4ef0583333de5acdb0f3
rolandCseh <csehroly@gmail.com>
2024-06-08 22:27:06 +0200

    Build ui with chakra and useQuery

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -23,9 +24,11 @@ type Todo struct {
 var collection *mongo.Collection
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file: ", err)
+	if os.Getenv("ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file: ", err)
+		}
 	}
 
 	mongoURL := os.Getenv("MONGODB_URI")
@@ -54,6 +57,15 @@ func main() {
 	app.Patch("/api/todos/:id", updateTodo)
 
 	port := os.Getenv("PORT")
+
+	if os.Getenv("ENV") == "production" {
+		app.Static("/", "./client/dist")
+	} else {
+		app.Use(cors.New(cors.Config{
+			AllowOrigins: "http://localhost:5173",
+			AllowHeaders: "Origin, Content-Type, Accept",
+		}))
+	}
 
 	log.Fatal(app.Listen(":" + port))
 }
